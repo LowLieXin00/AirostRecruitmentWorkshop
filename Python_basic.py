b1cc7d93873a625a5d4c66d961eb127a7e90b4a5
lowliexin <lowliexin00@gmail.com>
2021-11-11 13:42:03 +0800

    Fundamental of Python

---
+++ Python_basic.py
@@ -0,0 +1,118 @@
+# import the library
+import cv2
+import numpy as np
+
+###################
+#  Python syntax  #
+###################
+'''
+print("Hello World")
+
+##################
+#  Python lists  #
+##################
+
+# Python doesn't have built-in support for Array
+# Use external library like Numpy
+
+num_list = [3, 6, 9]
+print(len(num_list))
+num_array = np.array([3, 6, 9])
+print(type(num_list))
+print(type(num_array))
+print(num_array/3)
+
+######################
+#  Python operators  #
+######################
+
+num1 = 20
+num2 = 5
+
+# Arithmetic Operators
+print(20 + 5)
+print(20 % 5)
+print(20 ** 5)
+print(20 / 5)
+
+# Comparison Operators
+print(20 == 5)
+print(20 != 5)
+print(20 >= 5)
+
+#####################
+#  Python For Loop  #
+#  Python if..else  #
+#####################
+
+fruits = ['banana', 'apple', 'watermelon']
+
+for x in fruits:
+    print(x)
+    if x == 'banana':
+        fruits[0] = 'orange'
+
+# fruits.remove('watermelon')
+print(fruits)
+
+#######################
+#  Python While Loop  #
+#  Python function    #
+#######################
+
+def while_loop_1():
+    i = 1
+    while i <= 6:
+        print(i)
+        i += 1
+
+    else:
+        print("number greater than 6")
+
+while_loop_1()
+
+def print_add_func(x):
+    return print("This is number {}".format(x))
+
+def add_function(num=2):
+    # If you want to show value use print()
+    # If you want to return value use "return"
+    x = num + num
+    print(x)
+    return print_add_func(x)
+
+add_function(3)
+
+user_input = 0
+
+def while_loop_2(num):
+    while num <= 100:
+        if num == 5 or num == 6:
+            print("This is {}".format(num))
+        elif num == 7:
+            pass
+        elif num == 9:
+            break
+        else:
+            print(num)
+
+        num += 1
+
+    else:
+        print("Input number is greater than 100")
+
+while_loop_2(user_input)
+'''
+
+
+num_list = [3, 6, 9]
+
+num_array = np.array([3, 6, 9])
+print(len(num_array))
+print(type(num_list))
+print(type(num_array))
+print(num_array/3)
+
+
+
+
